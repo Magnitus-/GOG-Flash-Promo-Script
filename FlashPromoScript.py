a4f62dde8e09e0ce75c611ca78786208bf19fb68
Stephen Phoenix <phoeniixz@gmail.com>
2015-03-03 22:35:48 +0700

    Read patterns from file

--- FlashPromoScript.py
+++ FlashPromoScript.py
@@ -1,11 +1,11 @@
-#Author: Eric Vallee (eric_vallee2003@yahoo.ca)
+# Author: Eric Vallee (eric_vallee2003@yahoo.ca)
+# Modified by Stephen Phoenix
 import time
 import re
 import webbrowser
 import sys
-import string
 
-if sys.version_info.major==2:
+if sys.version_info.major == 2:
     import urllib2 as urllib2
     ExceptionClass = urllib2.URLError
 else:
@@ -13,12 +13,33 @@ else:
     import urllib.error
     ExceptionClass = urllib.error.URLError
 
-PATTERNS = ["Original Sin", "Second Sky", "Tendry", "Time Force", "Gunthro", "Defender of the Crown", "Wasteland 2", "Ethan Carter", "Friendship and Madness", "Starpoint Gemini", "Door Kickers", "Wings!", "X-Wing", "Tie Fighter", "Knights of the old Republic", "Sam & Max", "Last Federation", "Heretic Kingdoms", "Space Rangers HD", "Double Dragon", "Lords of Xulima", "Battlefront", "Rogue Squadron", "Grim Fandango", "Republic Commando", "Apotheon", "Sunless Sea", "Mortal Kombat", "Escapists", "Felghana", "Ys Origin", "Chronicles+", "Unwritten Tales 2", "New 'N' Tasty", "Republique", "Shelter 2", "Hotline Miami 2", "Shovel Knight", "Where is my", "Banner Saga", "Lifeless Planet"]
-VERBAL = True  #Set to True if you want feedback
+VERBAL = True  # Set to True if you want feedback
+
+
+def loadPatternFromFile():
+    patterns = []
+
+    with open("patterns.txt") as f:
+        lines = f.readlines()
+
+    patternIndex = 0
+
+    for line in lines:
+        if line.endswith("\n"):
+            line = line[:-1]
+
+        if len(line):
+            patterns.append(line)
+            print "{0:2}. {1}".format(patternIndex + 1, line)
+            patternIndex += 1
+
+    print "\n"
+    return patterns
+
 
 class InsomniaPromo2014(object):
     SourceURL = 'http://www.gog.com/springinsomnia/ajax/getCurrentOffer'
-    Delay = 4.0 #4 seconds
+    Delay = 4.0  # 4 seconds
 
     @staticmethod
     def _ProcessPatterns(Patterns):
@@ -31,16 +52,19 @@ class InsomniaPromo2014(object):
             if Match:
                 return True
         return False
-            
+
+
 class LateWinterPromo2015(InsomniaPromo2014):
     SourceURL = 'http://www.gog.com/doublesomnia/getdeals'
-    Delay = 10.0    #10 seconds
+    Delay = 10.0    # 10 seconds
+
 
 class PromoWarner(LateWinterPromo2015):
     AlarmURL = "http://www.youtube.com/watch?v=FoX7vd30zq8"
+
     def __init__(self, Patterns):
         self.PatternList = self._ProcessPatterns(Patterns)
-        
+
     def Warn(self):
         if VERBAL:
             print("Ready to poll the GOG server.")
@@ -60,13 +84,17 @@ class PromoWarner(LateWinterPromo2015):
                     print("Game found! The script will now sound the alarm!")
                 webbrowser.open(self.AlarmURL, new=2)
                 if VERBAL:
-                    print("Script will now end. Please, remove the game from the PATTERNS list if you bought it and restart the script.")
+                    print("Script will now end. Please, "
+                          "remove the game from the PATTERNS list "
+                          "if you bought it and restart the script.")
                 return
             elif VERBAL:
-                print("No game found. Will now sleep for "+str(self.Delay)+" seconds.")
+                print("No game found. Will now sleep for " + str(self.Delay)
+                      + " seconds.")
             time.sleep(self.Delay)
 
 
 if __name__=='__main__':
-    Warner = PromoWarner(PATTERNS)
-    Warner.Warn()   
+    patterns = loadPatternFromFile()
+    Warner = PromoWarner(patterns)
+    Warner.Warn()
